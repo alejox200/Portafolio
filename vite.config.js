f87068a9397f68827ca9d20c318abd9026a40b1e
alejox200 <alejox200@users.noreply.github.com>
2026-05-16 15:08:26 -0600

    Fix vite base path to /Portafolio/ to resolve 404 errors

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Esto soluciona la página en blanco en GitHub Pages
+  base: '/Portafolio/', // Ruta base correcta para GitHub Pages
   build: {
     outDir: 'docs' // Cambiamos 'dist' por 'docs' para GitHub Pages
   }
